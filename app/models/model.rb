f79f416bfc8623a419ac60ec3fc21a7df788f5e6
erinawidjaja <erina.widjaja@gmail.com>
2018-07-17 22:42:50 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,6 @@
-class Model
-  # Replace with Dog clas
-end
+# THIS IS WHERE THE RUBY HAPPENS (yay backend)
+def get_fortune
+  fortunes_array = ["You will become Melody's servant", "You will get straight A's", "You will get into USC", "You will see Angelina at Whole Foods", "Angelina will become Melody's roomate", "You will become a millionaire", "You will own a corgi"]
+  return fortunes_array.sample
+end 
+puts get_fortune
